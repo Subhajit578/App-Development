d497db228abe502685dfb40d0197bff42f676e91
Subhajit Guchhait <subhajit@iastate.edu>
2024-06-26 05:29:55 +0530

    Added all the projects

---
+++ GangaMarketPlace/Backend/src/main/java/GangaMarketplace/RunGangaMarketPlace.java
@@ -0,0 +1,14 @@
+package GangaMarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunGangaMarketPlace  {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunGangaMarketPlace.class, args);
+    }
+
+}
